6e545cd3577294be92ef8b1a31f5b40ddef3b111
Ajeetx24 <heropanther9@gmail.com>
2025-02-06 02:25:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,14 +1,14 @@
 import re
 from os import environ, getenv
 
-API_ID = environ.get("API_ID", "")
-API_HASH = environ.get("API_HASH", "")
+API_ID = environ.get("API_ID", "22677941")
+API_HASH = environ.get("API_HASH", "43b76b9bd08d90eb32eb06298dc18b1b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", "5935267941"))
 DATABASE_URL = environ.get("DATABASE_URL",  "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "ai")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
+DATABASE_NAME = environ.get("DATABASE_NAME", "MONTY")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001591180536"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001516086171"))
 FSUB = environ.get("FSUB", True)
 GOOGLE_API_KEY = environ.get('API_KEY', '')
 
